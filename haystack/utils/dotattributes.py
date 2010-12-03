c48e81361c7b97b0f135c0b582ff30b9fe8e2797
Roman Dolgiy <roman@bravetstudio.com>
2010-12-03 16:43:47 +0200

    Minor improvement and bugfix in utils.dotattributes

--- haystack/utils/dotattributes.py
+++ haystack/utils/dotattributes.py
@@ -1,6 +1,7 @@
 """
 This module provides smart way of getting attributes
-`obj.attr1.attr2` will be processed as `obj.attr1__attr2`
+`obj.attr1.attr2` will be processed as `obj.attr10_0_0attr2`
+if 0_0_0 is separator
 
 See `BaseResult` docstring for example.
 """
@@ -78,11 +79,11 @@ class BaseResult(object):
     >>> class Result(BaseResult):
     ...     def __init__(self, *args, **kwargs):
     ...         self.x = 35
-    ...         self.a0__0b = 40
+    ...         self.a0_0_0b = 40
     ...         self.y = 5
-    ...         self.y0__0a = 3
-    ...         self.y0__0b = 'hello'
-    ...         self.y0__0c0__0x = [1, 2, 3]
+    ...         self.y0_0_0a = 3
+    ...         self.y0_0_0b = 'hello'
+    ...         self.y0_0_0c0_0_0x = [1, 2, 3]
     ...         super(Result, self, *args, **kwargs)
     >>> r = Result()
     >>> r.x
@@ -107,17 +108,21 @@ class BaseResult(object):
     AttributeError: 'Result' object has no attribute 'y...d'
     >>> r.y.c.x
     [1, 2, 3]
+    >>> r.y__a
+    3
+    >>> r.y__c__x
+    [1, 2, 3]
     """
     def is_denorm_attr(self, name):
         return bool([k for k in self.__dict__.keys() if \
                          "%s%s" % (name, SEPARATOR) in k])
 
     def __getattribute__(self, name):
-        # TODO: if name is 'some__atr' replace it with
-        # 'someSEPARATORattr'
         super_method = super(BaseResult, self).__getattribute__
         if name in ('__dict__', 'is_denorm_attr'):
             return super_method(name)
+
+        # At first we need to check if this is denormalized attribute
         if self.is_denorm_attr(name):
             try:
                 attr = super_method(name)
@@ -126,6 +131,12 @@ class BaseResult(object):
                 attr = None
                 attr_type = Null
             return ResultAttrFactory(attr_type, self)(attr, name)
+
+        # If name is 'some__atr' replace it with
+        # 'someSEPARATORattr'
+        elif name.replace('__', SEPARATOR) in self.__dict__.keys():
+            return super_method(name.replace('__', SEPARATOR))
+
         else:
             return super_method(name)
 
